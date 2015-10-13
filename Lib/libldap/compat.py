65932b49f40fe4c37fa647ab90fd9e9a29010671
Yutaka Kamei <kamei@ykamei.net>
2015-10-13 14:37:28 +0900

    [WIP] Create python-ldap compatibility module

---
+++ Lib/libldap/compat.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2015 Yutaka Kamei
+"""libldap.compat module
+
+This module provides interfaces for python-ldap compatibility
+"""
+
+from _libldap import _LDAPError, _LDAPObject, _LDAPObjectControl
+
+
+def initialize(uri):
+    pass
