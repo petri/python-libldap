63da69a3d64e35b05d406c1675e09a4a31c5c2fc
Yutaka Kamei <kamei@ykamei.net>
2015-10-13 17:29:12 +0900

    Add functions in ldap.functions

--- Lib/libldap/compat.py
+++ Lib/libldap/compat.py
@@ -5,8 +5,39 @@
 This module provides interfaces for python-ldap compatibility
 """
 
-from _libldap import _LDAPError, _LDAPObject, _LDAPObjectControl
+from .core import LDAP
 
 
-def initialize(uri):
+def initialize(uri,
+               trace_level=None,
+               trace_file=None,
+               trace_stack_limit=None):
+    """
+    :param uri:
+        LDAP URI (e.g. `'ldap://localhost'`, `'ldaps://localhost'`, `'ldapi:///'`)
+    :param trace_level:
+        This parameter will be ignored
+    :param trace_file:
+        This parameter will be ignored
+    :param trace_stack_limit:
+        This parameter will be ignored
+    """
+    return LDAPObject(uri)
+
+
+def open(host, port=389):
+    initialize('ldap://%s:%d' % (host, port))
+
+
+def get_option(option):
+    ld = LDAP('ldap://localhost')
+    return ld.get_option(option, is_global=True)
+
+
+def set_option(option, invalue):
+    ld = LDAP('ldap://localhost')
+    return ld.set_option(option, invalue, is_global=True)
+
+
+class LDAPObject(LDAP):
     pass
